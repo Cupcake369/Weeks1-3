8acdf38f87762f762f09bb609e9b4e1bf9e524eb
moktanp <moktanp@OA-S141-12>
2026-05-06 15:32:01 -0400

    Changed xMax basically the max X value changed into the width of the screen.

--- Assets/Scripts/SquareMover.cs
+++ Assets/Scripts/SquareMover.cs
@@ -2,30 +2,37 @@ using UnityEngine;
 
 public class SquareMover : MonoBehaviour
 {
-    public float speed = 0.01f;
-    public float xMax = 5f;
-    public float xMin = -5f;
+    public Camera gameCamera;
+    public float speed;
+    float xMax;
+    float xMin = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      
+        xMax = Screen.width; // Get the width of the camera
+
 
     }
 
     // Update is called once per frame
     void Update()
-    {
-        Vector3 newPosition = transform.position;
-        newPosition.x += speed;
-        transform.position = newPosition;
-        if (transform.position.x > xMax)
+    {  
+        Vector3 newPosition = Camera.main.WorldToScreenPoint(transform.position); // convert the current position of the object to screen coordinates
+        //newPosition.z = 0; // Set z to 0 to keep the object on the same plane
+
+        if (newPosition.x > Screen.width)
         {
-            speed = -speed;
+            speed *= -1f;
         }
 
-        if (transform.position.x < xMin)
+        if (newPosition.x < Screen.width - Screen.width)
         {
-            speed = -speed;
+            speed *= -1f;
         }
+
+        Vector3 newPos = transform.position; // get the current position of the object in world coordinates
+        newPos.x += speed; // adds speed variable every fraem to the x coordinate of the new position
+
+        transform.position = newPos; // set the position of the object to the new position
     }
 }
