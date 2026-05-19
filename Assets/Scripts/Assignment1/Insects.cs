c99acbf0dda2bae5368a49d940faf2a4feed6984
Cupcake <prajjwalmoktan321@gmail.com>
2026-05-19 17:59:41 -0400

    Assignment1 Added Sprites
    Added sprites for the insects and a script to reference that sprite where it is from.

---
+++ Assets/Scripts/Assignment1/Insects.cs
@@ -0,0 +1,18 @@
+// using this from sprite from https://ponkpixels.itch.io/insect-enemies/download/eyJleHBpcmVzIjoxNzc5MjI3NTAxLCJpZCI6NTYzNjY3fQ%3d%3d%2ePomVoaQcclcHWH%2fBlRN8IEUMDbA%3d
+
+using UnityEngine;
+
+public class Insects : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
