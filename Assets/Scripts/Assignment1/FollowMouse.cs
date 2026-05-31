4090455a5dde22c5ee9b2071ab20422d85e70330
Cupcake <prajjwalmoktan321@gmail.com>
2026-05-30 22:17:48 -0400

    Just name fixes and added some comments

--- Assets/Scripts/Assignment1/FollowMouse.cs
+++ Assets/Scripts/Assignment1/FollowMouse.cs
@@ -13,7 +13,9 @@ public class FollowMouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //take mousepos and convert it into ingame position
         Vector2 currentMousePosition = gameCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        //take the pos that was converted and put that value as postion of the gameobject
         transform.position = currentMousePosition;
     }
 }
--- Assets/Scripts/Assignment1/Insects.cs
+++ Assets/Scripts/Assignment1/Insects.cs
@@ -6,6 +6,7 @@ using UnityEngine.Rendering;
 
 public class Insects : MonoBehaviour
 {
+
     public AnimationCurve curve;
     public AnimationCurve lerpCurve;
 
@@ -19,15 +20,17 @@ public class Insects : MonoBehaviour
     public bool isHover;
     public bool isRight;
     public float duration;
+
+    //timer for pluse
     float progress = 0f;
     public float timerDuration = 5f; // Duration of the timer in seconds
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+  
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+  
     void Update()
     {
         progress += Time.deltaTime; // increment the progress by the time elapsed since the last frame
@@ -48,9 +51,9 @@ public class Insects : MonoBehaviour
         Vector2 currentMousePosition = gameCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         Vector2 currentPostion = transform.position;
         Vector2 newPos = transform.position;
-
+        //get distance between mouse and insect
         float distance = Vector2.Distance(currentMousePosition, currentPostion);
-        
+        //when mouse is close to the insect isHover becomes true
         if (distance < distanceFromMouse)
         {
             isHover = true;
@@ -62,6 +65,8 @@ public class Insects : MonoBehaviour
         //    //Debug.Log("The mouse is far from the insect");
         //}
 
+        //when isHover is true
+        //timer will start used to lerp the position from minDistance to maxDistance
         if (isHover == true)
         {
             flyMove += Time.deltaTime;
@@ -73,8 +78,9 @@ public class Insects : MonoBehaviour
             //isRight = true;
 
         }
-
-        if(flyMove >= 1f)
+        //when fluMove timer is more than 1
+        //reset the timer while flipping the min and max distance to make the insect fly back and forth
+        if (flyMove >= 1f)
         {
             isHover = false;
             flyMove = 0f;
